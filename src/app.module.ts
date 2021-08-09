0ded3933f6a79ee4080b1c4eea9e9422cce64800
Daniele Moscati <d.moscati@nbsgroup.it>
2021-08-09 18:56:10 +0200

    Risolto bug lettura immagine per salvataggio

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,13 @@ import { AppRoutingModule } from './app/modules/app-routing.module';
 import { AppComponent } from './app/components/app/app.component';
 
 import { Camera } from "@ionic-native/camera/ngx";
+import { File } from "@ionic-native/file/ngx";
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, Camera],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, Camera, File],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,3 +1,4 @@
+import { toBase64String } from '@angular/compiler/src/output/source_map';
 import { Injectable } from '@angular/core';
 import { Camera, CameraOptions } from "@ionic-native/camera/ngx";
 import { File } from "@ionic-native/file/ngx";
@@ -8,13 +9,18 @@ import { IPhoto } from '../interfaces/photo.interface';
   providedIn: 'root',
 })
 
+/*
+https://stackoverflow.com/questions/52055126/not-allowed-to-load-local-resource-ionic-3-android/55934321#55934321
+https://stackoverflow.com/questions/48878116/convert-cordova-image-picker-results-to-base64-format-ionic
+*/
 export class PhotoService {
   /**Array contenente le foto scattate */
   public photos: IPhoto[] = [];
 
+  /**Variabile che identifica la finestra corrente */
   private win: any = window;
 
-  /** Opzioni per la fotocamera */
+  /**Opzioni per la fotocamera */
   private cameraOptions: CameraOptions = {
     quality: 100,
     destinationType: this.camera.DestinationType.FILE_URI,
@@ -22,22 +28,59 @@ export class PhotoService {
     mediaType: this.camera.MediaType.PICTURE
   }
 
-  constructor(private camera: Camera) { }
+  constructor(private camera: Camera, private file: File) { }
 
   /**Metodo che salva una foto quando viene scattata */
   public async addNewPhotoToGallery(){
     this.camera.getPicture(this.cameraOptions).then(
       (imageData) => {
         this.photos.unshift({
-          filepath: "generic",
+          filepath: 'edai',
           webviewPath: imageData,
           convertedFileSrc: this.win.Ionic.WebView.convertFileSrc(imageData)
         });
-        console.log(this.photos);
+        this.savePicture(imageData);
       },
       (error) => {
         console.log(error);
       }
     )
+  }
+
+  private async savePicture(imageData){
+    let imageName = imageData.split('/').pop();
+    let path = imageData.substring(0, imageData.lastIndexOf("/") + 1);
+    this.file.readAsDataURL(path, imageName).then(
+      (base64File) => {
+        this.convertToBlobImage(base64File.replace('data:image/jpeg;base64,','')).then(
+          (blobImage) => {
+            console.log(blobImage);
+            let fileName = new Date().getTime() + '.jpeg';
+            this.file.writeFile('', fileName, blobImage, {replace: true}).then(
+              (fileWritten) => {
+                console.log(fileWritten)
+              },
+              (error) => {
+                console.log("Errore scrittura file");
+                console.log(error);
+              }
+            );
+          },
+          (error) => {
+            console.log("Errore conversione Blob");
+            console.log(error);
+          }
+        );
+      },
+      (error) => {
+        console.log("Errore lettura immagine");
+        console.log(error);
+      }
+    )
+  }
+
+  private async convertToBlobImage(base64File){
+    const base64Response = await fetch(`data:image/jpeg;base64,${base64File}`);
+    return await base64Response.blob();
   }
 }
